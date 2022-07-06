e6c0e5177aee95449c0ca450bf27ff35193012fd
jae <wogud1514@naver.com>
2022-07-06 21:06:27 +0900

    시작

---
+++ src/test/java/jjfactory/hodol/HodolApplicationTests.java
@@ -0,0 +1,13 @@
+package jjfactory.hodol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HodolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
